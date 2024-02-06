a05a8124c97e8ddf05099c4d55d12c9d3d6c9090
12Maggie11 <144775422+12Maggie11@users.noreply.github.com>
2024-02-06 20:31:51 +0800

    Create setup.py

---
+++ reportpy/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='reportpy',
+    version='1.0.3',
+    description='Play game',
+    author='Maggie Tsai',
+    packages=find_packages(),
+    install_requires=['google-api-python-client'],
+)
